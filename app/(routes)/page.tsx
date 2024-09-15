72acf26257dcdf627f341bbb6cce6a93d50b840c
Bhim Prasad Adhikari <bhimprasadadhikari98@gmail.com>
2024-09-15 14:44:37 +0545

    ...

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -4,7 +4,7 @@ import Billboard from '@/components/billboard';
 import Container from '@/components/ui/container';
 import ProductsList from '@/components/product-list';
 import getProducts from '@/actions/get-products';
-export const revalidate = 0;
+// export const revalidate = 0;
 const HomePage = async () => {
   const products = await getProducts({isFeatured:true});
   const billboard = await getBillboard('66e1b17ee5a4dd025ef90820');
