00ff51d9eeaa7ae7b30960022a9543e51349ecae
Bhim Prasad Adhikari <bhimprasadadhikari98@gmail.com>
2024-09-15 18:03:10 +0545

    new file:   app/(routes)/product/page.tsx

---
+++ app/(routes)/product/page.tsx
@@ -0,0 +1,14 @@
+
+const CategoryPage = async () => {
+  
+  return (
+  
+      <div className='space-y-10 pb-10'>
+       
+         <div className='flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8'>
+         catogory
+         </div>
+      </div>
+  );
+};
+export default CategoryPage;
