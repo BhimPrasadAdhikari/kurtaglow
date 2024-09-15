058a2092078435627b6773d3ce9d25f4f6ea9a4e
Bhim Prasad Adhikari <bhimprasadadhikari98@gmail.com>
2024-09-15 13:53:26 +0545

    url modified

--- actions/get-billboard.tsx
+++ actions/get-billboard.tsx
@@ -7,7 +7,8 @@
 
 import { Billboard as BillboardType } from '@/types';
 
-const baseUrl=`${process.env.NEXT_PUBLIC_API_URL}/billboards`;
+// const baseUrl=`${process.env.NEXT_PUBLIC_API_URL}/billboards`;
+const baseUrl="https://admin-dashboard-eight-lemon.vercel.app/api/66e1b12be5a4dd025ef9081f/billboards"
 const getBillboard = async (id:string):Promise<BillboardType> => {
  const url=baseUrl+'/'+id;
  const res= await fetch(url);
--- actions/get-category.tsx
+++ actions/get-category.tsx
@@ -7,7 +7,9 @@
 
 import { Category as CategoryType } from '@/types';
 
-const url=`${process.env.NEXT_PUBLIC_API_URL}/categories`;
+// const url=`${process.env.NEXT_PUBLIC_API_URL}/categories`;
+const url ="https://admin-dashboard-eight-lemon.vercel.app/api/66e1b12be5a4dd025ef9081f/categories";
+
 const getCategory = async (id:string):Promise<CategoryType> => {
  const res= await fetch(`${url}/${id}`);
  return res.json()
