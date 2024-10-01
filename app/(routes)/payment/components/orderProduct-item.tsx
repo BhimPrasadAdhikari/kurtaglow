90ff462fc061690fe06b1464b9e21374a36996b0
Bhim Prasad Adhikari <bhimprasadadhikari98@gmail.com>
2024-10-01 16:22:10 +0545

    reverted ..

--- app/(routes)/payment/components/orderProduct-item.tsx
+++ app/(routes)/payment/components/orderProduct-item.tsx
@@ -14,35 +14,32 @@ const OrderProductItem:React.FC<OrderProductItemProps>=async({data,quantity})=>{
         <div className="flex gap-10">
                 <CarouselPluginOrder data={product.images} />
                 {/* <CartItemInfo product={product}/> */}
-        <div className="relative pl-12 gap-3 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:pr-0">
-            <div>
-              <p className="text-neutral-400">Name</p>
-              <p>{product.name}</p>
-               <p className="text-wrap text-sm">{product.detail}</p>
-            </div>
-            <div className="flex gap-4">
-            <div>
-              <p className="text-neutral-400">Color</p>
-              <p>{product.color.name}</p>
+                <div className="relative pl-4 gap-3 sm:grid sm:grid-cols-2 sm:gap-x-6 md:pl-12">
+  <div className="flex flex-col mb-4 sm:mb-0">
+    <p className="text-neutral-400">Name</p>
+    <p className="font-semibold">{product.name}</p>
+    <p className="text-wrap text-sm">{product.detail}</p>
+  </div>
+  <div className="grid grid-cols-2 gap-4">
+    <div>
+      <p className="text-neutral-400">Color</p>
+      <p className="font-semibold">{product.color.name}</p>
+    </div>
+    <div>
+      <p className="text-neutral-400">Size</p>
+      <p className="font-semibold">{product.size.name}</p>
+    </div>
+    <div>
+      <p className="text-neutral-400">Price</p>
+      <Currency value={product.price} />
+    </div>
+    <div>
+      <p className="text-neutral-400">Quantity</p>
+      <p className="font-semibold">{quantity}</p>
+    </div>
+  </div>
+</div>
 
-            </div>
-            <div>
-              <p className="text-neutral-400">Size</p>
-              <p>{product.size.name}</p>
-
-            </div>
-            <div>
-              <p className="text-neutral-400">Price</p>
-              <Currency value={product.price}/>
-
-            </div>
-            <div>
-              <p className="text-neutral-400">Quantity</p>
-              <p>{quantity}</p>
-
-            </div>
-            </div>
-        </div>
         </div>
     )
 }
