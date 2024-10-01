ad22afbd32e4503d7641cd0a1e5d3ed78b99cbb3
Bhim Prasad Adhikari <bhimprasadadhikari98@gmail.com>
2024-10-01 21:47:39 +0545

    make catogory/id public

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,13 @@
 import { authMiddleware } from '@clerk/nextjs/server';
 
 export default authMiddleware({
-  publicRoutes: ['/', '/category', '/product:path','/cart'], // Routes that can be accessed without signing in
+  publicRoutes: ['/', '/category', '/product:path','/cart','/category:path'], // Routes that can be accessed without signing in
 });
 
 export const config = {
   matcher: [
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     '/(api|trpc)(.*)',
-    
+
   ],
 };
