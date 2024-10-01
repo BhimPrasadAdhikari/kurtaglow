b1e73f658ef8a5beb4f714f46fd2a73fbf478316
Bhim Prasad Adhikari <bhimprasadadhikari98@gmail.com>
2024-10-01 15:35:00 +0545

    made carousal responsive

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -7,37 +7,38 @@ import CarouselPlugin from '@/components/carousalPulgin';
 import HomePageSkeleton from "@/components/HomePageSkelton";
 
 export const revalidate = 0;
+
 const HomePage = async () => {
- try{ 
-  const products = await getProducts({isFeatured:true});
-  // const billboard = await getBillboard('66e1b17ee5a4dd025ef90820');
-  const billboards = await getBillboards();
+  try { 
+    const products = await getProducts({ isFeatured: true });
+    const billboards = await getBillboards();
 
-  return (
-    <Container>
-      <div className='space-y-10 pb-10'>
-        {/* <Billboard data={billboard} /> */}
-        <Suspense fallback={<HomePageSkeleton/>}>
-        <div className=' flex items-center justify-center mx-20'>
-        <CarouselPlugin data={billboards}/>
+    return (
+      <Container>
+        <div className='space-y-10 pb-10'>
+          {/* Billboard (Commented out for now) */}
+          {/* <Billboard data={billboard} /> */}
 
+          <Suspense fallback={<HomePageSkeleton />}>
+            <div className='flex items-center justify-center mx-4 md:mx-20'>
+              <CarouselPlugin data={billboards} />
+            </div>
+          </Suspense> 
+
+          <Suspense fallback={<HomePageSkeleton />}>
+            <div className='flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8'>
+              <ProductsList title="Featured Products" data={products} />
+            </div>
+          </Suspense>
         </div>
-      </Suspense> 
-      <Suspense fallback={<HomePageSkeleton/>}>
-
-         <div className='flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8'>
-          <ProductsList title="Featured Products" data={products} />
-         </div>
-      </Suspense>
-
-      </div>
-    </Container>
-  );}catch(err){
-    console.log("HOME_PAGE",err)
+      </Container>
+    );
+  } catch (err) {
+    console.log("HOME_PAGE", err);
     return (
       <>Something Went Wrong</>
-    )
+    );
   }
-
 };
+
 export default HomePage;
--- components/carousalPulgin.tsx
+++ components/carousalPulgin.tsx
@@ -1,47 +1,52 @@
-'use client'
-import * as React from "react"
-import Autoplay from "embla-carousel-autoplay"
-import { Billboard } from "@/types"
+'use client';
+import * as React from "react";
+import Autoplay from "embla-carousel-autoplay";
+import { Billboard } from "@/types";
 import {
   Carousel,
   CarouselContent,
   CarouselItem,
   CarouselNext,
   CarouselPrevious,
-} from "@/components/ui/carousel"
-import Image from "next/image"
-interface CarousalProps{
-  data:Billboard[];
+} from "@/components/ui/carousel";
+import Image from "next/image";
+
+interface CarouselProps {
+  data: Billboard[];
 }
-const CarouselPlugin:React.FC<CarousalProps> =({data}) =>{
-  
+
+const CarouselPlugin: React.FC<CarouselProps> = ({ data }) => {
   const plugin = React.useRef(
     Autoplay({ delay: 2000, stopOnInteraction: false })
-  )
+  );
 
   return (
-    <Carousel
-      plugins={[plugin.current]}
-      className="w-3/4 h-60 "
-      // onMouseEnter={plugin.current.stop}
-      // onMouseLeave={plugin.current.reset}
+    <div className="w-full h-60 sm:h-80 md:h-96 lg:h-112 relative overflow-hidden"> {/* Full width and responsive height */}
+      <Carousel
+        plugins={[plugin.current]}
+        className="w-full h-full" // Full width and height
       >
-      <CarouselContent className="h-60 w-auto p-0">          
-        {data.map((billboard, index) => (
-      <CarouselItem key={index}>
-            <div className="p-0 ">
-              <div>
-                <div className="flex justify-center aspect-auto">
-                 <Image src={billboard.imageUrl} height={700} width={700} alt="image" priority />
-                </div>              
+        <CarouselContent className="h-full w-full p-0">          
+          {data.map((billboard, index) => (
+            <CarouselItem key={index} className="h-full w-full">
+              <div className="h-full w-full flex justify-center items-center"> {/* Centering the image */}
+                <Image 
+                  src={billboard.imageUrl} 
+                  height={700} 
+                  width={700} 
+                  alt="image" 
+                  priority 
+                  className="object-cover h-full w-full" // Ensure the image covers the area
+                />
               </div>
-            </div>
-          </CarouselItem>)
-        )}
-      </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
-    </Carousel>
-  )
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious className="absolute left-4 top-1/2 transform -translate-y-1/2 z-10" />
+        <CarouselNext className="absolute right-4 top-1/2 transform -translate-y-1/2 z-10" />
+      </Carousel>
+    </div>
+  );
 }
+
 export default CarouselPlugin;
